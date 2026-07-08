4dddc1504969b9dd82bdf78263c7c836d514a29c
Suvigya Shankhdhar <shankhdharsuvigya499@gmail.com>
2026-07-08 10:24:11 +0530

    Add realloc and calloc interface

--- include/my_alloc.h
+++ include/my_alloc.h
@@ -7,7 +7,6 @@ void *my_alloc(size_t size);
 void my_free(void *ptr); 
 void *my_realloc(void *ptr, size_t size); 
 void *my_calloc(size_t nmemb, size_t size); 
-int   my_posix_memalign(void **memptr, size_t alignment, size_t size);
 void  my_malloc_stats(void);
 
 #endif
--- src/api.c
+++ src/api.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include "../include/my_alloc.h"
 #include "internals.h"
 #include "thread_local_cache.h"
@@ -42,3 +44,46 @@ void my_free(void *ptr) {
     }
 }
 
+
+void *my_calloc(size_t nmemb, size_t size) {
+    if (nmemb == 0 || size == 0){
+        return NULL; 
+    }
+    size_t total_size = nmemb * size; 
+    void *ptr = my_alloc(total_size);
+    if (total_size > PAGE_SIZE){
+        return memset(ptr, 0, total_size);
+    }
+    return ptr;
+}
+
+
+void *my_realloc(void *ptr, size_t size) {
+    if (ptr == NULL) {
+        return my_alloc(size); 
+    }
+    if (size == 0) {
+        my_free(ptr); 
+        return NULL; 
+    }
+    size_t old_size = 0; 
+    if ((uintptr_t *)ptr >= (uintptr_t *)heap_start && (uintptr_t *)ptr < (uintptr_t *)heap_end) {
+        old_size = get_class_size(index_from_ptr(ptr));
+    }
+    else {
+        LargeBlock *large_block = (LargeBlock* )((char*) ptr - LARGE_BLOCK_SIZE); 
+        old_size = large_block->size - LARGE_BLOCK_SIZE; 
+    }
+
+    if (size <= old_size) {
+        return ptr; 
+    }
+     
+    void *new_ptr = my_alloc(size); 
+    if (new_ptr == NULL) {
+        return NULL; 
+    }
+    memcpy(new_ptr, ptr, old_size);  
+    my_free(ptr); 
+    return new_ptr; 
+}
